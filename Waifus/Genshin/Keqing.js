372e11568284e8b37dce4b31dc91aa7522d17b5c
MarkAnthonyO <83286299+elFailSteve@users.noreply.github.com>
2022-06-20 21:12:36 -0500

    0.0.2

--- Waifus/Genshin/Keqing.js
+++ Waifus/Genshin/Keqing.js
@@ -5,7 +5,7 @@ const Keqing = new Waifu(
     "",
     "https://www.pinterest.com.mx/pin/726627721146591257/",
     "https://i.pinimg.com/564x/74/76/2d/74762d42c23bd153e516df5c0f4f494b.jpg",
-    "Keqing",
+    "keqing",
     ""
 )
 
